aef2ebc64e1acc9dc17132d8ff072fd6344baabf
monika <monikamohite@gmail.com>
2017-10-03 20:54:41 +0530

    part1 completed by monika

---
+++ Paras-CollectionBackEnd/src/main/java/career/Paras_CollectionBackEnd/App.java
@@ -0,0 +1,13 @@
+package career.Paras_CollectionBackEnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
